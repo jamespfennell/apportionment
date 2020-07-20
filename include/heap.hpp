53a6b6d094c2faede9880072b689fc38d815627e
james <jamespfennell@gmail.com>
2020-07-20 18:39:23 -0400

    Cleaned up the heap

--- include/heap.hpp
+++ include/heap.hpp
@@ -3,27 +3,14 @@
 using namespace std;
 
 template<typename T>
-struct MaxHeapNode {
-    T element;
-    long double weight;
-
-    MaxHeapNode(T element, long double weight) { // TODO this is not Javay?
-        this->element = element;
-        this->weight = weight;
-    }
-};
-
-
-template<typename T>
 class Heap {
-   vector<MaxHeapNode<T>> nodes2;
-   long numElements = 0;  // TODO: remove
 
     struct Node {
         T element;
         long double weight;
     };
 
+   vector<Node> nodes2;
     long parentIndex(long childIndex) {
         if (childIndex == 0) {
             return -1;
@@ -52,7 +39,7 @@ class Heap {
     }
 
     void swap(long index1, long index2) {
-        MaxHeapNode<T> element = std::move(this->nodes2[index1]);
+        Node element = std::move(this->nodes2[index1]);
         this->nodes2[index1] = std::move(this->nodes2[index2]);
         this->nodes2[index2] = std::move(element);
     }
@@ -70,12 +57,11 @@ class Heap {
    T pop() {
        // Check for no elements! Otherwise get a seg fault
        long lastIndex = this->nodes2.size() - 1;
-       if (this->numElements > 1) {
+       if (nodes2.size() > 1) {
            this->swap(0, lastIndex);
        }
        T result2 = std::move((this->nodes2[lastIndex]).element); // [lastIndex].element;
        this->nodes2.pop_back();
-       this->numElements -= 1;
 
        long parentIndex = 0;
         long leftChildIndex = this->leftChildIndex(parentIndex);
@@ -110,9 +96,8 @@ class Heap {
     // Could also have addCopy
     // or pass by regulare reference and do the move itself
    void add(T element, double weight) {
-       this->numElements = this->numElements + 1;
-       nodes2.push_back(MaxHeapNode<T>{element, weight});
-        long childIndex = this->numElements - 1;
+       nodes2.push_back(Node{element, weight});
+        long childIndex = nodes2.size() - 1;
         long parentIndex = this->parentIndex(childIndex);
         while (parentIndex >= 0 and (this->nodes2[parentIndex]).weight < (this->nodes2[childIndex]).weight ) {
             this->swap(parentIndex, childIndex);
